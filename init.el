c3f66a951b360036d2db3fef1938fa10b8ecc136
Yuhong Zhong <nanua.zqz@gmail.com>
2017-11-12 15:54:32 +0800

    configed org-mode

--- init.el
+++ init.el
@@ -1,5 +1,12 @@
 ﻿;;acc start-up
+(defvar default-file-name-handler-alist file-name-handler-alist)
+(setq file-name-handler-alist nil)
 (setq gc-cons-threshold 100000000)
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            "Restore defalut values after init"
+            (setq file-name-handler-alist default-file-name-handler-alist)
+            (setq gc-cons-threshold 800000)))
 
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
@@ -14,7 +21,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (jdee cdlatex auctex flycheck elpy company-jedi nlinum github-theme github-modern-theme))))
+    (jdee matlab-mode cdlatex auctex flycheck elpy company-jedi nlinum github-theme github-modern-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -22,6 +29,10 @@
  ;; If there is more than one, they won't work right.
  )
 
+;;Smooth mouth scroll
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
+(setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
+(setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
  
 ;;Set scroll setting
 (setq scroll-step 1
@@ -69,12 +80,42 @@
 (electric-pair-mode t)
 
 ;;Enable Flycheck
-(when (require 'flycheck nil t)
-  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
-  (add-hook 'elpy-mode-hook 'flycheck-mode))
+;(when (require 'flycheck nil t)
+;  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+;  (add-hook 'elpy-mode-hook 'flycheck-mode))
 
-;;ban welcome page
+;;Ban welcome page
 (setq inhibit-startup-message t)
+
+;;Pair paren
+(electric-pair-mode 1)
+
+;;Set up Server
+(server-start)
+
+;;Resolve Chinese encode problem
+(set-language-environment 'Chinese-GB)
+(set-default buffer-file-coding-system 'utf-8-unix)
+(set-default-coding-systems 'utf-8-unix)
+(setq-default pathname-coding-system 'euc-cn)
+(setq file-name-coding-system 'euc-cn)
+(prefer-coding-system 'cp950)
+(prefer-coding-system 'gb2312)
+(prefer-coding-system 'cp936)
+(prefer-coding-system 'gb18030)
+;(prefer-coding-system 'utf-16le-with-signature)
+(prefer-coding-system 'utf-16)
+(prefer-coding-system 'utf-8-dos)
+(prefer-coding-system 'utf-8-unix)
+
+;;Reread file from disk
+(defun refresh-file ()  
+  (interactive)  
+  (revert-buffer t (not (buffer-modified-p)) t))  
+(global-set-key [(control f5)] 'refresh-file)  
+
+;;Enable delete mode
+(delete-selection-mode 1)
 
 ;;Config AUCTex
 (setq TeX-auto-save t)
@@ -85,21 +126,41 @@
 (setq-default LaTeX-command-style '((""
 "%(PDF)%(latex)%(extraopts) %S%(PDFout)")
 ))
+(setq-default fill-column 100)
 (setq preview-gs-command "C:/Program Files (x86)/gs/gs9.22/bin/gswin32c.exe")
+(add-hook 'LaTeX-mode-hook 'turn-on-auto-fill)
 (add-hook 'LaTeX-mode-hook 
           (lambda()
              (add-to-list 'TeX-command-list '("XeLaTeX" "%`xelatex%(mode)%' %t" TeX-run-TeX nil t))
              (setq TeX-command-default "XeLaTeX")
-             (setq TeX-save-query nil)
-             (setq TeX-show-compilation t)))
+             (setq TeX-save-query nil)))
+(electric-pair-mode)
+(add-hook 'LaTeX-mode-hook
+          '(lambda ()
+            (define-key LaTeX-mode-map (kbd "(") 'self-insert-command)))
 			 
 ;;Config CDLatex
 (add-hook 'LaTeX-mode-hook 'turn-on-cdlatex)
 (setq cdlatex-env-alist
-      '(("enumerate" "\\begin{enumerate}\n?\n\\end{enumerate}\n" nil)
-	    ("itemize" "\\begin{itemize}\n?\n\\end{itemize}\n" nil)
-        ("figure" "\\begin{figure}\n\\centering\n\\includegraphics[width=12cm]{}\n\\caption{}\n\label{fig:}\n\\end{figure}\n" nil)))
+      '(("enumerate" "\\begin{enumerate}\n?\n\\end{enumerate}" nil)
+	    ("itemize" "\\begin{itemize}\n?\n\\end{itemize}" nil)
+		("item" "\\item{?}" nil)
+		("displaymath" "\\begin{displaymath}\n?\n\\end{displaymath}" nil)
+		("align" "\\begin{align*}\n?\n\\end{align*}" nil)
+        ("figure" "\\begin{figure}\n\\centering\n\\includegraphics[width=12cm]{?}\n\\caption{}\n\\label{fig:}\n\\end{figure}" nil)
+		("table" "\\begin{center}\n\\begin{tabular}\n?\n\\end{tabular}\n\\end{center}" nil)
+		("tabularx" "\\begin{table}\n\\begin{tabularx}{\\textwidth}{?}\n\\toprule[1.5pt]\n\n\\midrule[1pt]\n\n\\bottomrule[1.5pt]\n\\end{tabularx}\n\\end{table}" nil)
+		("longtable" "\\begin{longtable}{?}\n\\toprule[1.5pt]\n\n\\midrule[1pt]\n\\endfirsthead\n\\toprule[1.5pt]\n\n\\endhead\n\\bottomrule[1.5pt]\n\\endfoot\n\n\\end{longtable}" nil)))
 (setq cdlatex-command-alist
   '(("enu" "Insert enumerate env"   "" cdlatex-environment ("enumerate") t nil)
     ("ite" "Insert itemize env" "" cdlatex-environment ("itemize") t nil)
-	("fig" "Insert figure env" "" cdlatex-environment ("figure") t nil)))
+	("it" "Insert item env" "" cdlatex-environment ("item") t nil)
+	("fig" "Insert figure env" "" cdlatex-environment ("figure") t nil)
+	("tab" "Insert table env" "" cdlatex-environment ("table") t nil)
+	("ltab" "Insert longtable env" "" cdlatex-environment ("longtable") t nil)
+	("utab" "Insert tabularx env" "" cdlatex-environment ("tabularx") t nil)
+	("dismath" "Insert displaymath env" "" cdlatex-environment ("displaymath") t nil)
+	("ali" "Insert align env" "" cdlatex-environment ("align") t nil)))
+
+;;Config org-mode
+(setq org-startup-indented t)
